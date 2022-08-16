d51797f9d7a8a0db45dcd68b46a75ce3bad83eaa
Christopher Fauconnier <christopherfauconnier@gmail.com>
2022-08-16 12:51:52 +0200

    fix Profile function

--- frontend/src/Components/Profile/Profile.js
+++ frontend/src/Components/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import AuthService from "../services/auth.service";
-import '../Login/Login.css';
-import './Profile.css';
+import "../Login/Login.css";
+import "./Profile.css";
 
 export default class Profile extends Component {
   constructor(props) {
@@ -10,22 +10,19 @@ export default class Profile extends Component {
     this.state = {
       currentUser: {},
       isLoaded: false,
-      points: 0
+      points: 0,
     };
   }
   getPoints() {
-    AuthService.getCurrentUser()
-        .then(() => {
-        });
+    AuthService.getCurrentUser().then(() => {});
   }
   componentDidMount() {
-    AuthService.getCurrentUser()
-        .then((result) => {
-            this.setState({
-                currentUser: result.data,
-                isLoaded:true
-            });
-        });
+    AuthService.getCurrentUser().then((result) => {
+      this.setState({
+        currentUser: result.data,
+        isLoaded: true,
+      });
+    });
     this.intervalPoints = setInterval(this.getPoints, 1000);
   }
 
@@ -33,16 +30,14 @@ export default class Profile extends Component {
     clearInterval(this.intervalPoints);
   }
   delete() {
-    AuthService.deleteUser()
-      .then(() => {
-        window.location.href = "/Articles";
-      })
+    AuthService.deleteUser().then(() => {
+      window.location.href = "/Articles";
+    });
   }
   disconnect() {
-    AuthService.deleteCurrentUser()
-      .then(() => {
-        window.location.href = "/Login";
-      })
+    AuthService.deleteCurrentUser().then(() => {
+      window.location.href = "/Login";
+    });
   }
 
   render() {
@@ -53,74 +48,79 @@ export default class Profile extends Component {
         style={{
           color,
           backgroundColor: color,
-          height: 1
+          height: 1,
         }}
       />
     );
-    if(!this.state.isLoaded){
-      return (
-        <div>chargement...</div>
-      )
+    if (!this.state.isLoaded) {
+      return <div>chargement...</div>;
     }
     return (
       <div className="profil">
-      <div className="container">
-        <header className="jumbotron">
-          <h3>
-            <strong>{currentUser.name}</strong>
-          </h3>
-        </header>
-        <p>
-          <strong>Email:</strong>{" "}
-          {currentUser.mail}
-        </p>
-        <p>
-          <strong>Role:</strong>{" "}
-          {currentUser.role}
-        </p>
-        <p>
-          <strong>Points:</strong>{" "}
-          {currentUser.points}
-        </p>
-        <p>
-          <strong>Mes cadeaux</strong>{" "}
-          <div>
-            {currentUser.userOffers.map((offer) => (
+        <div className="container">
+          <header className="jumbotron">
+            <h3>
+              <strong>{currentUser.name}</strong>
+            </h3>
+          </header>
+          <p>
+            <strong>Email:</strong> {currentUser.mail}
+          </p>
+          <p>
+            <strong>Role:</strong> {currentUser.role}
+          </p>
+          <p>
+            <strong>Points:</strong> {currentUser.points}
+          </p>
+          <p>
+            <strong>Mes cadeaux</strong>{" "}
+            <div>
+              {currentUser.userOffers.map((offer) => (
                 <p>
-                <div className="title-coupon">{offer.name}</div>
-                <div className="description"><i>{offer.description}</i></div>
-                <ColoredLine color="grey"/>
+                  <div className="title-coupon">{offer.name}</div>
+                  <div className="description">
+                    <i>{offer.description}</i>
+                  </div>
+                  <ColoredLine color="grey" />
                 </p>
-            ))}
-          </div>
-        </p>
-      </div>
-      <div className="d-flex justify-content-around">
-        <button
-                //type="submit"
-                className="btn btn-dark btn-bg profile-button"
-                id="Déconnexion"
-                onClick={this.disconnect}
-                >
-                    Déconnexion
+              ))}
+            </div>
+          </p>
+        </div>
+        <div className="d-flex justify-content-around">
+          <button
+            //type="submit"
+            className="btn btn-dark btn-bg profile-button"
+            id="Déconnexion"
+            onClick={this.disconnect}
+          >
+            Déconnexion
           </button>
           <button
-                //type="submit"
-                className="btn btn-dark btn-bg profile-button"
-                id="Historique"
-                onClick={this.disconnect}
-                >
-                    Historique des commandes
+            //type="submit"
+            className="btn btn-dark btn-bg profile-button"
+            id="Historique"
+            onClick={this.disconnect}
+          >
+            Historique des commandes
           </button>
         </div>
         <div className="d-flex justify-content-center">
           <button
-                //type="submit"
-                className="btn btn-dark btn-bg profile-button"
-                id="Suppression"
-                onClick={() => {if (window.confirm("Êtes-vous sûr de vouloir supprimer votre compte Bangoo?")) {this.delete}}}
-                >
-                    Supprimer mon compte
+            //type="submit"
+            className="btn btn-dark btn-bg profile-button"
+            id="Suppression"
+            onClick={() => {
+              if (
+                window.confirm(
+                  "Êtes-vous sûr de vouloir supprimer votre compte Bangoo?"
+                )
+              ) {
+                this.delete();
+              }
+            }}
+          >
+            Supprimer mon compte
           </button>
         </div>
       </div>
